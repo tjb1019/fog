1942936ac0016c6c33c184f158729d8fbc347f4a
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2016-04-21 12:19:35 -0700

    Added image and shared storage unit tests

--- tests/oneandone/test_images.rb
+++ tests/oneandone/test_images.rb
@@ -5,36 +5,49 @@ Fog.mock!
 
 class TestImage < Minitest::Test
   
+  def self.test_order
+   :alpha
+  end
+
   def setup
 
     # Establish Connection
     @compute = Fog::Compute::OneAndOne.new({
-      :oneandone_api_key => '675fbe491b27896b57e76867604f8255'
+      :oneandone_api_key => 'APIKEY'
     })
-
-    # Create Image
-    image = @compute.create_image(server_id: 'ServerID',
-      name: 'Test Image', frequency: 'ONCE', num_images: 1)
-    
-    @image_id = image.body['id']
 
   end
 
 
-  def test_list_images
+  def test_a_create_image
+
+    # Perform Request
+    response = @compute.create_image(server_id: 'ServerID',
+      name: 'Test Image', frequency: 'ONCE', num_images: 1)
+    
+    @@image_id = response.body['id']
+
+    # Assertions
+    assert_equal response.body['name'], 'Test Image'
+    assert_equal response.body['frequency'], 'ONCE'
+    assert_equal response.body['num_images'], 1
+
+  end
+
+  def test_b_list_images
     
     # Perform Request
     response = @compute.list_images
 
     # Assertions
-    assert_equal response.body[0]['server_id'], 'ServerID'
+    assert_equal response.body.length, 1
 
   end
 
-  def test_update_image
+  def test_c_update_image
 
     # Perform Request
-    response = @compute.update_image(image_id: @image_id,
+    response = @compute.update_image(image_id: @@image_id,
       name: 'New Name', frequency: 'WEEKLY')
 
     # Assertions
@@ -43,20 +56,20 @@ class TestImage < Minitest::Test
 
   end
 
-  def test_get_image
+  def test_d_get_image
 
     # Perform Request
-    response = @compute.get_image(@image_id)
+    response = @compute.get_image(@@image_id)
 
     # Assertions
-    assert_equal response.body['id'], @image_id
+    assert_equal response.body['id'], @@image_id
 
   end
 
-  def test_delete_image
+  def test_e_delete_image
 
     # Perform Request
-    response = @compute.delete_image(@image_id)
+    response = @compute.delete_image(@@image_id)
 
     # Assertions
     assert_equal response.body, 'The requested image has been deleted.'
